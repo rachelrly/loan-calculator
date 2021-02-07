399d2b7bb39371d7eef330d0f75016acf60381a0
Rachel <r.a.reill18@gmail.com>
2021-02-07 13:34:38 -0600

    changed font and removed instructions from page

--- src/components/LoanCalculator.js
+++ src/components/LoanCalculator.js
@@ -10,9 +10,6 @@ import '../css/LoanCalculator.css'
 function LoanCalculator() {
   return (
     <section className='loan-calculator-wrapper'>
-    <div className='instructions-wrapper'>
-    <p>Add a loan amount, interest rate, and loan term. Once all three fields have been filled out, the app will calculate the monthly payment and total interest.</p>
-    </div>
       <form className='calculator-wrapper'>
         <LoanAmountInput />
         <InterestRateInput />
